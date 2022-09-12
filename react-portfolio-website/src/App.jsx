4952579d65ce2fae66d616ca2cd4cedc6e5a8fb7
auqib nazir <auqibnazir@gmail.com>
2022-09-12 23:06:01 +1000

    basic template for portfolio website created header, footer, contact etc all major components created

---
+++ react-portfolio-website/src/App.jsx
@@ -0,0 +1,33 @@
+import React from 'react'
+import Header from './components/header/Header'
+import Nav from './components/nav/Nav'
+import About from './components/about/About'
+import Experience from './components/experience/Experience'
+import Services from './components/services/Services'
+import Testimonials from './components/testimonials/Testimonials'
+import Portfolio from './components/portfolio/Portfolio'
+import Contact from './components/contact/Contact'
+import Footer from './components/footer/Footer'
+
+
+const App = () => {
+  return (
+    <>
+      <Header />
+      <Contact />
+      <Nav />
+      <About />
+      <Experience />
+      <Services />
+      <Testimonials />
+      <Portfolio />
+      <Footer />
+
+
+
+
+    </>
+  )
+}
+
+export default App
