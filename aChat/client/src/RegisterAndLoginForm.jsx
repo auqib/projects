ca64864dac4f3b51eed776154a2ddda1089f35e3
auqib nazir <auqibnazir@gmail.com>
2023-06-02 21:59:35 +1000

    readme.md file added

--- aChat/client/src/RegisterAndLoginForm.jsx
+++ aChat/client/src/RegisterAndLoginForm.jsx
@@ -7,7 +7,7 @@ const RegisterAndLoginForm = () => {
   const [password, setPassword] = useState("");
   const { usernameContext, setUsernameContext, id, setId } =
     useContext(Context);
-  const [isLoginOrRegister, setLoginOrRegister] = useState("register");
+  const [isLoginOrRegister, setLoginOrRegister] = useState("login");
 
   async function handleSubmit(ev) {
     ev.preventDefault();
@@ -35,9 +35,10 @@ const RegisterAndLoginForm = () => {
           placeholder="password"
           className="block w-full rounded-md p-2 mb-2 border"
         />
-        <button className="bg-blue-500 text-white block w-full rounded-md p-2">
+        <button class="text-white bg-gradient-to-r from-cyan-500 to-blue-500 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-cyan-300 dark:focus:ring-cyan-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2 w-full">
           {isLoginOrRegister === "register" ? "Register" : "Login"}
         </button>
+
         <div className="text-center mt-2">
           {isLoginOrRegister === "register" && (
             <div>
@@ -58,8 +59,11 @@ const RegisterAndLoginForm = () => {
                 onClick={() => {
                   setLoginOrRegister("register");
                 }}
+                class="relative inline-flex items-center justify-center p-0.5 mb-2 mr-2 overflow-hidden text-sm font-medium text-gray-900 rounded-lg group bg-gradient-to-br from-green-400 to-blue-600 group-hover:from-green-400 group-hover:to-blue-600 hover:text-white dark:text-white focus:ring-4 focus:outline-none focus:ring-green-200 dark:focus:ring-green-800"
               >
-                Register
+                <span class="relative px-5 py-2.5 transition-all ease-in duration-75 bg-white dark:bg-gray-900 rounded-md group-hover:bg-opacity-0">
+                  Register
+                </span>
               </button>
             </div>
           )}
